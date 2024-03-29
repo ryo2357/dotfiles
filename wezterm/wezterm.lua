a564194eabe76a723e7ae0a92cfff9c0618743ae
ryo2357 <miyawakir@hosokawa-yoko.co.jp>
2024-03-29 15:45:49 +0900

    weztermの調整

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = {}
 
@@ -7,8 +8,28 @@ if wezterm.config_builder then
 end
 
 -- カラースキームの設定
-config.color_scheme = 'AdventureTime'
+-- config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Dark Violet'
 
 config.default_prog = { 'nu' }
+-- trueで日本語切り替えを可能にする 設定の反映に再起動が必要
+config.use_ime=true
+
+-- ショートカットキー設定
+config.keys = {
+  { key = "k", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
+}
+
+config.skip_close_confirmation_for_processes_named = {
+  'bash',
+  'sh',
+  'zsh',
+  'fish',
+  'tmux',
+  'nu',
+  'cmd.exe',
+  'pwsh.exe',
+  'powershell.exe',
+}
 
 return config
